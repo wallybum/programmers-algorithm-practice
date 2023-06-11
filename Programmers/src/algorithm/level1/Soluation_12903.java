2425dc5ae6932ed5012eb2bde2952187ad4ebb62
WallyBum <elvvis93@gmail.com>
2023-06-11 22:09:24 +0900

    주석 작성

--- Programmers/src/algorithm/level1/Soluation_12903.java
+++ Programmers/src/algorithm/level1/Soluation_12903.java
@@ -1,7 +1,13 @@
 package algorithm.level1;
 
-import java.util.stream.IntStream;
-
+/**
+ * <문제 설명>
+ * 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+ * 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+ * 
+ * <재한사항>
+ *  s는 길이가 1 이상, 100이하인 스트링입니다.
+ */
 public class Soluation_12903 {
 	public static void main(String[] args) {
 		run();
@@ -9,14 +15,13 @@ public class Soluation_12903 {
 
 	private static void run() {
 		String s = "abcde";
-//		String s = "qwer";
 		String answer = "";
-		
-		if(s.length() %2 == 0) {
-			answer = s.substring(s.length()/2-1, s.length()/2+1);
-		}else {
-			answer = s.substring(s.length()/2, s.length()/2+1);
+
+		if (s.length() % 2 == 0) {
+			answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+		} else {
+			answer = s.substring(s.length() / 2, s.length() / 2 + 1);
 		}
-		System.out.println("answer : "+answer);
+		System.out.println("answer : " + answer);
 	}
 }
