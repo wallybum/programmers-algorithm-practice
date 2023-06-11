903168732bb5a013182b7dfb7331707b2fb7030a
WallyBum <elvvis93@gmail.com>
2023-06-11 22:12:41 +0900

    두 정수 사이의 합

--- Programmers/src/algorithm/level1/Soluation_12912.java
+++ Programmers/src/algorithm/level1/Soluation_12912.java
@@ -1,5 +1,16 @@
 package algorithm.level1;
 
+/**
+ * <문제 설명>
+ * 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요. 
+ * 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+ * 
+ * <제한 조건>
+ * a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+ * a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다. a와 b의 대소관계는 정해져있지 않습니다.
+ *
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12912
+ */
 public class Soluation_12912 {
 	public static void main(String[] args) {
 		run();
@@ -10,14 +21,14 @@ public class Soluation_12912 {
 		int a = 3;
 		int b = 5;
 		long answer = 0;
-		
-		if(a >= b) {
-			for(int i=b; i <=a; i++) {
-				answer +=i;
+
+		if (a >= b) {
+			for (int i = b; i <= a; i++) {
+				answer += i;
 			}
-		}else {
-			for(int i =a; i <=b; i++) {
-				answer +=i;
+		} else {
+			for (int i = a; i <= b; i++) {
+				answer += i;
 			}
 		}
 		System.out.println(answer);
